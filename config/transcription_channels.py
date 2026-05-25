bba9ca0aee5fb4b384b0e16d4477f24c5ddba728
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2026-05-25 14:22:23 -0600

    Add @MitosyMentes to global and forced channel lists

--- config/transcription_channels.py
+++ config/transcription_channels.py
@@ -1,4 +1,7 @@
 TRANSCRIPTION_CHANNEL_URLS = [
     "https://www.youtube.com/@bilinkis",
     "https://www.youtube.com/@Veritasium",
+    "https://www.youtube.com/@noesposible",
+    "https://www.youtube.com/@Alan-Flores",
+    "https://www.youtube.com/@MitosyMentes",
 ]
